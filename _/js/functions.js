a1d873aa35d1a1d6ae8c44d75ee71ab305d8165a
mjchamplin <mjchamplin@gmail.com>
2012-07-07 18:26:33 -0500

    Contact Form Dependencies, index html>php

--- _/js/functions.js
+++ _/js/functions.js
@@ -5,7 +5,50 @@
 /* trigger when page is ready */
 $(document).ready(function (){
 
-	// your functions go here
+
+	// validate signup form on keyup and submit
+	var validator = $("#contactform").validate({
+		rules: {
+			contactname: {
+				required: true,
+				minlength: 2
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			subject: {
+				required: true,
+				minlength: 2
+			},
+			message: {
+				required: true,
+				minlength: 10
+			}
+		},
+		messages: {
+			contactname: {
+				required: "Please enter your name",
+				minlength: jQuery.format("Your name needs to be at least {0} characters")
+			},
+			email: {
+				required: "Please enter a valid email address",
+				minlength: "Please enter a valid email address"
+			},
+			subject: {
+				required: "You need to enter a subject!",
+				minlength: jQuery.format("Enter at least {0} characters")
+			},
+			message: {
+				required: "You need to enter a message!",
+				minlength: jQuery.format("Enter at least {0} characters")
+			}
+		},
+		// set this class to error-labels to indicate valid fields
+		success: function(label) {
+			label.addClass("checked");
+		}
+	});
 
 });
 
