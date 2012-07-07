270e9855290e6fcf316a0447cd8e0c1a4b6e3c57
mjchamplin <mjchamplin@gmail.com>
2012-07-07 18:26:50 -0500

    Contact Form Dependencies, index html>php

---
+++ _/php/contactengine.php
@@ -0,0 +1,49 @@
+<?php
+//If the form is submitted
+if(isset($_POST['submit'])) {
+
+	//Check to make sure that the name field is not empty
+	if(trim($_POST['contactname']) == '') {
+		$hasError = true;
+	} else {
+		$name = trim($_POST['contactname']);
+	}
+
+	//Check to make sure that the subject field is not empty
+	if(trim($_POST['subject']) == '') {
+		$hasError = true;
+	} else {
+		$subject = trim($_POST['subject']);
+	}
+
+	//Check to make sure sure that a valid email address is submitted
+	if(trim($_POST['email']) == '')  {
+		$hasError = true;
+	} else if (!eregi("^[A-Z0-9._%-]+@[A-Z0-9._%-]+\.[A-Z]{2,4}$", trim($_POST['email']))) {
+		$hasError = true;
+	} else {
+		$email = trim($_POST['email']);
+	}
+
+	//Check to make sure comments were entered
+	if(trim($_POST['message']) == '') {
+		$hasError = true;
+	} else {
+		if(function_exists('stripslashes')) {
+			$comments = stripslashes(trim($_POST['message']));
+		} else {
+			$comments = trim($_POST['message']);
+		}
+	}
+
+	//If there is no error, send the email
+	if(!isset($hasError)) {
+		$emailTo = 'your@emailaddress.com'; //Put your own email address here
+		$body = "Name: $name \n\nEmail: $email \n\nSubject: $subject \n\nComments:\n $comments";
+		$headers = 'From: My Site <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+
+		mail($emailTo, $subject, $body, $headers);
+		$emailSent = true;
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,176 @@
+<!doctype html>
+
+<!--[if lt IE 7 ]> <html class="ie ie6 no-js" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="ie ie7 no-js" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="ie ie8 no-js" lang="en"> <![endif]-->
+<!--[if IE 9 ]>    <html class="ie ie9 no-js" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<!-- the "no-js" class is for Modernizr. -->
+
+<head id="www-sitename-com" data-template-set="html5-reset">
+
+	<meta charset="utf-8">
+	
+	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<title></title>
+	
+	<meta name="title" content="">
+	<meta name="description" content="">
+	<!-- Google will often use this as its description of your page/site. Make it good. -->
+	
+	<meta name="google-site-verification" content="">
+	<!-- Speaking of Google, don't forget to set your site up: http://google.com/webmasters -->
+	
+	<meta name="author" content="Your Name Here">
+	<meta name="Copyright" content="Copyright Your Name Here 2011. All Rights Reserved.">
+
+	<!-- Dublin Core Metadata : http://dublincore.org/ -->
+	<meta name="DC.title" content="Project Name">
+	<meta name="DC.subject" content="What you're about.">
+	<meta name="DC.creator" content="Who made this site.">
+	
+	<!--  Mobile Viewport Fix
+	j.mp/mobileviewport & davidbcalhoun.com/2010/viewport-metatag 
+	device-width : Occupy full width of the screen in its current orientation
+	initial-scale = 1.0 retains dimensions instead of zooming out if page height > device height
+	maximum-scale = 1.0 retains dimensions instead of zooming in if page width < device width
+	-->
+	<!-- Uncomment to use; use thoughtfully!
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	-->
+
+	<link rel="shortcut icon" href="_/img/favicon.ico">
+	<!-- This is the traditional favicon.
+		 - size: 16x16 or 32x32
+		 - transparency is OK
+		 - see wikipedia for info on browser support: http://mky.be/favicon/ -->
+		 
+	<link rel="apple-touch-icon" href="_/img/apple-touch-icon.png">
+	<!-- The is the icon for iOS's Web Clip.
+		 - size: 57x57 for older iPhones, 72x72 for iPads, 114x114 for iPhone4's retina display (IMHO, just go ahead and use the biggest one)
+		 - To prevent iOS from applying its styles to the icon name it thusly: apple-touch-icon-precomposed.png
+		 - Transparency is not recommended (iOS will put a black BG behind the icon) -->
+	
+	<!-- CSS: screen, mobile & print are all in the same file -->
+	<link rel="stylesheet" href="_/css/style.css">
+	
+	<!-- all our JS is at the bottom of the page, except for Modernizr. -->
+	<script src="_/js/modernizr-1.7.min.js"></script>
+
+</head>
+
+<body>
+
+	<header>		
+		<div class="logo middle">
+			<img src="_/img/logo.png" class="middle" />	
+		</div>				
+	</header>
+
+	<article id="upper">
+		<div class="wrapper-inner middle">		
+			<div id="left-column">
+			<h1>Commercial</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>				</div>
+			<div id="right-column">
+				<h1>Community</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>		
+			<div class="clear"></div>
+		</div>
+	</article>
+	
+	<article id="lower">
+		<section class="middle">
+			<h2>Want to know more?</h2>
+			<h3>Get in touch.</h3>			
+		<div id="contactWrapper" role="form">
+	
+		<h1 role="heading">Send us a message</h1>
+
+		<?php if(isset($hasError)) { //If errors are found ?>
+			<p class="error">Please check if you've filled all the fields with valid information and try again. Thank you.</p>
+		<?php } ?>
+
+		<?php if(isset($emailSent) && $emailSent == true) { //If email is sent ?>
+			<div class="success">
+				<p><strong>Email Successfully Sent!</strong></p>
+				<p>Thank you for using our contact form <strong><?php echo $name;?></strong>! Your email was successfully sent and we 'll be in touch with you soon.</p>
+			</div>
+		<?php } ?>
+
+		<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" id="contactform">
+			<div class="stage clear">
+				<label for="name"><strong>Name: <em>*</em></strong></label>
+				<input type="text" name="contactname" id="contactname" value="" class="required" role="input" aria-required="true" />
+			</div>
+
+			<div class="stage clear">
+				<label for="email"><strong>Email: <em>*</em></strong></label>
+				<input type="text" name="email" id="email" value="" class="required email" role="input" aria-required="true" />
+			</div>
+
+			<div class="stage clear">
+				<label for="subject"><strong>Subject: <em>*</em></strong></label>
+				<input type="text" name="subject" id="subject" value="" class="required" role="input" aria-required="true" />
+			</div>
+
+			<div class="stage clear">
+				<label for="message"><strong>Message: <em>*</em></strong></label>
+				<textarea rows="8" name="message" id="message" class="required" role="textbox" aria-required="true"></textarea>
+			</div>
+								
+			<p class="requiredNote"><em>*</em> Denotes a required field.</p>
+			
+			<input type="submit" value="Send Message" name="submit" id="submitButton" title="Click here to submit your message!" />
+		</form>
+		
+	</div>
+		
+		
+		</section>
+		
+		
+		
+		<section class="middle">b</section>	
+	</article> 
+
+	
+	<footer>
+		<p>&copy; 2012 flash flood -LOGO- website by michael j. champlin</p>		
+	</footer>
+
+<!-- here comes the javascript -->
+
+<!-- Grab Google CDN's jQuery. fall back to local if necessary -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+<script>window.jQuery || document.write("<script src='_/js/jquery-1.5.1.min.js'>\x3C/script>")</script>
+
+<!-- Form Validation -->
+<script src="http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.pack.js" type="text/javascript"></script>
+
+<!-- this is where we put our custom functions -->
+<script src="_/js/functions.js"></script>
+
+<!-- Asynchronous google analytics; this is the official snippet.
+	 Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.
+	 
+<script>
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-XXXXXX-XX']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+-->
+  
+</body>
+</html>
